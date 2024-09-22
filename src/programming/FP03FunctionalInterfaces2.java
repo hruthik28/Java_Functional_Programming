d29579fb54a18d0bade9edc7d00206b944753a49
hruthik28 <64793294+hruthik28@users.noreply.github.com>
2024-09-22 17:15:48 +0530

    Update FP03FunctionalInterfaces2.java

--- src/programming/FP03FunctionalInterfaces2.java
+++ src/programming/FP03FunctionalInterfaces2.java
@@ -20,16 +20,16 @@ public class FP03FunctionalInterfaces2 {
 
 		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 
-		//input -> boolean output
+		//1 input -> boolean output
 		Predicate<Integer> isEvenPredicate = (Integer x) -> x % 2 == 0;
 
-		//input -> output
+		//1 input -> 1 output
 		Function<Integer, Integer> squareFunction = x -> x * x;
 		
-		//input -> output
+		//1 input -> 1 output
 		Function<Integer, String> stringOutpuFunction = x -> x + " ";
 
-		//input -> no output
+		//1 input -> no output
 		Consumer<Integer> sysoutConsumer = x -> System.out.println(x);
 
 		//2 inputs -> 1 output, all 3 are of same dataType
@@ -42,22 +42,26 @@ public class FP03FunctionalInterfaces2 {
 		};
 		
 		//System.out.println(randomIntegerSupplier.get());
-		
+
+		//1 input -> 1 output of same type
 		UnaryOperator<Integer> unaryOperator = x -> 3 * x;
 		System.out.println(unaryOperator.apply(10));
 		
+		//2 input -> boolean output
 		BiPredicate<Integer, String> biPredicate = (number,str) -> {
 			return number<10 && str.length()>5;
 		};
 		
 		System.out.println(biPredicate.test(10, "in28minutes"));
-		
+
+		//2 input -> 1 output
 		BiFunction<Integer, String, String> biFunction = (number,str) -> {
 			return number + " " + str;
 		};
 		
 		System.out.println(biFunction.apply(15, "in28minutes"));
-		
+
+		//2 input -> no output
 		BiConsumer<Integer, String> biConsumer = (s1,s2) -> {
 			System.out.println(s1);
 			System.out.println(s2);
